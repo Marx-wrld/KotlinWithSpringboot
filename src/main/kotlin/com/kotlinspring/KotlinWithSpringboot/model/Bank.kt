46d8773f679104d4dfef3d9400e3e8ccea8fbf9f
Ian Marx <iongonga@kabarak.ac.ke>
2023-12-08 23:29:34 +0300

    Kotlin's data class - In Spring Boot, and when building REST APIs, these are commonly (mapped to) data transfer objects, or DTOs for short. In this video, you create the data layer for your Spring Boot application by implementing a Bank entity. First, we do it as you would (have to) do it in Java, then refactor it to a simple Kotlin data class. Spring Boot has a default object mapper that knows how to map objects of such entities to and from JSON.

---
+++ src/main/kotlin/com/kotlinspring/KotlinWithSpringboot/model/Bank.kt
@@ -0,0 +1,10 @@
+package com.kotlinspring.KotlinWithSpringboot.model
+
+data class Bank(
+    //A data class automatically creates a standard implementation of equals toHashCode and toString
+    //when you declare a var or a val kotlin declares a getter for you
+        val accountNumber: String,
+        val trust: Double,
+        val transactionFee: Int
+    //Kotlin we use primary constructor which is declared up here
+)
