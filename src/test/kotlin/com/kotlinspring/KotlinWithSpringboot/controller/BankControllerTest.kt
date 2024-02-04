2d2ccd713d3c3a06f9c58722d286a66a6ef673d1
Ian Marx <iongonga@kabarak.ac.ke>
2024-02-04 16:38:52 +0300

    Web layer - web layer is responsible for receiving request and returning the proper response. Everything in between is handled by the other layers, where the actual business logic happens. In Spring Boot, this layer is represented by controllers (annotated with @Controller or @RestController), named after the model-view-controller (MVC) pattern. We're creating the initial web layer for our RESTful Spring Boot webservice by defining a BankController. Additionally, we create our first integration test that covers all layers of the application to see if everything plugs together as it should. For this, we use Spring’s MockMvc for automated testing of REST APIs.

--- src/test/kotlin/com/kotlinspring/KotlinWithSpringboot/controller/BankControllerTest.kt
+++ src/test/kotlin/com/kotlinspring/KotlinWithSpringboot/controller/BankControllerTest.kt
@@ -1,9 +1,7 @@
 package com.kotlinspring.KotlinWithSpringboot.controller
 
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
